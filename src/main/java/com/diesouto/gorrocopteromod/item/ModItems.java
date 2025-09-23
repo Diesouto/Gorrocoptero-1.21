080caa99b8361b0e5a0aa45c2770d80ccad12ae2
Diesouto <diegosouto2000@gmail.com>
2025-09-23 23:33:05 +0200

    Initial commit

---
+++ src/main/java/com/diesouto/gorrocopteromod/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.diesouto.gorrocopteromod.item;
+
+import com.diesouto.gorrocopteromod.GorrocopteroMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, GorrocopteroMod.MODID);
+
+    public static final RegistryObject<Item> HELIX = ITEMS.register("helix",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
